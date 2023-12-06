a67ed474dc928c3fa3cbd8a15336554eb85457ae
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-12-05 21:50:44 -0300

    condicao

---
+++ Condicao/src/Condicao.java
@@ -0,0 +1,14 @@
+public class Condicao {
+    public static void main(String[] args)  {
+        
+        int numero = 19;
+
+        if (numero % 2 == 0) {
+            System.out.println("Numero é par!");
+        } 
+        else {
+            System.out.println("Numero é impar!");     
+        }
+
+    }
+}
