dc228e7504b6fa5da11d8bd51f4d1ab2d5881469
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-12-05 21:36:54 -0300

    ex strings

---
+++ ExString/src/ExString.java
@@ -0,0 +1,14 @@
+public class ExString {
+
+    public static void main(String[] args) {
+
+        String nome = "Diego";
+        int comprimento = 0;
+
+        comprimento = nome.length();
+
+        System.out.println("O nome " + nome + " tem " + comprimento + " letras");
+
+    }
+
+}
