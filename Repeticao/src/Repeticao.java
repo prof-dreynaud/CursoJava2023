39401ce9f9fcfa46616da07694908d827b1ea465
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-12-05 21:44:05 -0300

    repeticao

---
+++ Repeticao/src/Repeticao.java
@@ -0,0 +1,11 @@
+public class Repeticao {
+    public static void main(String[] args)  {
+        
+        int i = 0;
+
+        for (i=1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+    }
+}
