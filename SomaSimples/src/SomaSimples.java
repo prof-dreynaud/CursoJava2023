a440f5feb230d548953746f0c1ebe2ebbc1798f6
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-12-07 19:43:52 -0300

    SomaSimples

---
+++ SomaSimples/src/SomaSimples.java
@@ -0,0 +1,16 @@
+public class SomaSimples {
+    public static void main(String[] args)  {
+        
+        int nro1 = 5;
+        int nro2 = 3;
+        int soma = 0;
+        int sub = 0;
+
+        soma = nro1 + nro2;
+        sub = nro1 - nro2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + sub);
+
+    }
+}
