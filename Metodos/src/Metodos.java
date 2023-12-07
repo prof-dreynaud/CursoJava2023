e3a3e23700dc8f174b641c48526c4c92da591f08
Prof Diego <diego.reynaud@edu.sc.senai.br>
2023-12-07 20:13:35 -0300

    Metodo

---
+++ Metodos/src/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+
+    //método que retorna soma de dois nros
+    static int somar(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        int resultado = somar(3,13);
+
+        System.out.println("O resultado é: " + resultado);
+
+    }
+}
